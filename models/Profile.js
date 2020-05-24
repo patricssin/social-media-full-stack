cb9215deb2a3714f101ec59d8eb696e891924cd0
patricssin <bianbianmomo@126.com>
2020-05-24 14:13:49 +0800

    udpate profile

---
+++ models/Profile.js
@@ -0,0 +1,111 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    require: true,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        require: true,
+      },
+      company: {
+        type: String,
+        require: true,
+      },
+      location: {
+        type: String,
+        require: true,
+      },
+      from: {
+        type: String,
+        require: true,
+      },
+      to: {
+        type: String,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true,
+      },
+      degree: {
+        type: String,
+        require: true,
+      },
+      fieldofstudy: {
+        type: String,
+        require: true,
+      },
+      from: {
+        type: String,
+        require: true,
+      },
+      to: {
+        type: String,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Profile = mongoose.model('porfile', ProfileSchema);
