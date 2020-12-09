40af2b1a9ebfbeb6d2f8823a480fe720314e594c
patricssin <bianbianmomo@126.com>
2020-12-09 21:27:28 +0800

    set up components

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Register = (props) => {
@@ -100,7 +101,7 @@ const Register = (props) => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
