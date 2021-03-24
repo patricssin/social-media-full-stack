d0987f8e1999791d21f318a705cece55b5376b92
patricssin <bianbianmomo@126.com>
2021-03-24 21:50:56 +0800

    edit profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import {connect} from 'react-redux'
 import Spinner from '../layout/Spinner'
 import {getCurrentProfile} from '../../actions/profile'
 
+import DashboardActions from './DashboardActions'
+
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {loading, profile}}) => {
   useEffect(() => {
     getCurrentProfile()
@@ -17,7 +19,9 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {loading, profile}
     </p>
     {
       profile !== null ? (
-        <Fragment>has</Fragment>
+        <Fragment>
+          <DashboardActions />
+        </Fragment>
       ) : (
         <Fragment>
           <p>You have not yet setup a profile, please add some info</p>
